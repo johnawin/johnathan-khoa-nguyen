af2d98c7a379bf59fa4aa724a117108c0938622b
Johnathan <johnawin95@gmail.com>
2022-08-23 05:27:09 -0700

    added homepage and aboutme page, created layout and css modules

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Welcome to my Website</p>
+    </Layout>
+  )
+}
+
+
+
+export default AboutPage
