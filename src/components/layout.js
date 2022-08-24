2743a06565c80cff09888b54358fff9ba58a2910
Johnathan <johnawin95@gmail.com>
2022-08-23 19:26:55 -0700

    moved main,links, and added profile picture

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,16 +5,16 @@ import {container} from './layout.module.css'
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
         </ul>
       </nav>
-      <main>
-        <h1>{pageTitle}</h1>
-        {children}
-      </main>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from "react"
-
 import Layout from '../components/layout'
+import {StaticImage} from 'gatsby-plugin-image'
+import * as Images from '../images'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>Home Page</p>
+    <Layout pageTitle="NGUYEN">
+      <p>Johnathan Khoa</p>
+      <StaticImage
+        src="../images/IMG_0345.JPG"
+        />
     </Layout>
   )
 }
