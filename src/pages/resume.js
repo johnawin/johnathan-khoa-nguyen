fc694739b2aec116eba160eca3752d6d23ef3bbc
Johnathan <johnawin95@gmail.com>
2022-08-27 02:48:00 -0700

    added download link for resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import Layout from '../components/layout'
-
+import ReactDOM from "react-dom/client"
+import {Link} from 'react-router-dom'
 const ResumePage = () =>{
   return (
     <Layout pageTitle="Resume">
       <p>My Resume will be linked here for download</p>
+      <a href="../dl/AA. Johnathan Nguyen Resume.pdf" download>Resume</a>
     </Layout>
   )
 }
